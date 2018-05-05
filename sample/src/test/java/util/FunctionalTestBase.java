b98f2116c9777ad1f4e3fc3273f0f76c3a044b38
hamaspyur91 <petrosyan.91@gmail.com>
2018-05-05 14:42:19 +0400

    updating base test

--- sample/src/test/java/util/FunctionalTestBase.java
+++ sample/src/test/java/util/FunctionalTestBase.java
@@ -10,7 +10,7 @@ public class FunctionalTestBase {
 	public WebDriver driver ;
 	final String URL = "https://google.com";
 	String chromeDriverProp = "webdriver.chrome.driver";
-	String driverPath="C:\\Users\\hovsepyana\\Downloads\\chromedriver.exe";
+	String driverPath="C:\\Users\\Hamaspyur Petrosyan\\Documents\\drivers\\chromedriver.exe";
 	
 	@BeforeClass
 	public void setup() {
